7dd5a130106eb9a1822c8e9a0cb34bf0461c6da0
Tabernet <muppets@gmail.com>
2017-04-25 14:28:28 -0400

    Update LateGame.js

--- LateGame.js
+++ LateGame.js
@@ -5,6 +5,7 @@ desc:'An Econ mod that adds a later game uses to heavily stockpiled resources',
 engineVersion:1,
 manifest:'LateGame.js',
 requires:['Default dataset*'],
+sheets:{'LateGameSheet':'/Lategame.png'},
 func:function()
 {
 	//The idea in this simple mod is to add a means of adding use to items that otherwise just fill storage
@@ -20,12 +21,12 @@ func:function()
 //new Water Purification Unit:
 		new G.Unit({
 		name:'Water Purification System',
-		desc:'@Turns 750 [muddy water] into [water], using 10 [coal] and 50 [stone]',
+		desc:'@Turns 100 [muddy water] into [water], using 50 [coal] and 50 [stone]',
 		icon:[22,7],
 		cost:{'basic building materials':1000,'soft metal ingot':20},
 		use:{'land':1},
 		effects:[
-			{type:'convert',from:{'stone':50,'muddy water':750,'coal':10},into:{'water':100},every:10}
+			{type:'convert',from:{'stone':50,'muddy water':100,'coal':50},into:{'water':100},every:10}
 		],
 		category:'production',
 		req:{'water purification':true},
@@ -37,7 +38,21 @@ func:function()
 			//adding a new effect to carpenter workshop 
 	G.getDict('carpenter workshop').effects.push({type:'convert',from:{'log':1},into:{'stick':5},every:3,mode:'sticks'});
 	
-
+	new G.Unit({
+		name:'Appartment',
+		desc:'@provides 50 [housing]<>A towering edifice of humanity .',
+		icon:[0,0,'LateGameSheet'],
+		cost:{'basic building materials':250,'cut stone':500 },
+		use:{'land':1},
+		//require:{'worker':3,'metal tools':3},
+		effects:[
+			{type:'provide',what:{'housing':50}},
+			{type:'waste',chance:0.01/1000}
+		],
+		req:{'city planning':true},
+		category:'housing',
+	});
+	
 	
 	
 }
