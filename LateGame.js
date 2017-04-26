bb3bd3bcf9ef128f45edc52e7eb1e7d539548833
Tabernet <muppets@gmail.com>
2017-04-26 11:31:36 -0400

    Update LateGame.js

--- LateGame.js
+++ LateGame.js
@@ -5,7 +5,7 @@ desc:'An Econ mod that adds a later game uses to heavily stockpiled resources',
 engineVersion:1,
 manifest:'LateGame.js',
 requires:['Default dataset*'],
-sheets:{'LateGameSheet':'/Lategame.png'},
+sheets:{'LateGameSheet':https://raw.githubusercontent.com/Tabernet/StickMod/master/Lategame.png},
 func:function()
 {
 	//The idea in this simple mod is to add a means of adding use to items that otherwise just fill storage
@@ -13,7 +13,7 @@ func:function()
 		//new technology which alows Water Purification:
 	new G.Tech({
 		name:'water purification',
-		desc:'@Convert 750 [muddy water] can now be filtered in to [water] with [coal] and crushed [stone]s//Water once deadly can now be safely consumed',
+		desc:'@[muddy water] can now be filtered in to [water] with [coal] and crushed [stone]s//Water once deadly can now be safely consumed',
 		icon:[25,3],
 		cost:{'insight':30},
 		req:{'city planning':true},
@@ -21,7 +21,7 @@ func:function()
 //new Water Purification Unit:
 		new G.Unit({
 		name:'Water Purification System',
-		desc:'@Turns 100 [muddy water] into [water], using 50 [coal] and 50 [stone]',
+		desc:'@Turns 250 [muddy water] into [water], using 50 [coal] and 50 [stone]',
 		icon:[22,7],
 		cost:{'basic building materials':1000,'soft metal ingot':20},
 		use:{'land':1},
@@ -30,7 +30,7 @@ func:function()
 		],
 		category:'production',
 		req:{'water purification':true},
-		limitPer:{'land':25},
+		
 	});
 
 		//adding a new mode to carpenter workshop 
@@ -53,7 +53,31 @@ func:function()
 		category:'housing',
 	});
 	
-	
+	new G.Unit({
+		name:'Mass Grave',
+		desc:'@provides 100 [burial spot], in which the [corpse,dead] are automatically interred one by one@graves with buried corpses decay over time, freeing up land for more graves<>A simple grave dug into the earth, where the dead may find rest.//Burying your dead helps prevent [health,disease] and makes your people slightly [happiness,happier].',
+		icon:[0,1,'LateGameSheet'],
+		cost:{},
+		use:{'land':1},
+		//require:{'worker':1,'stone tools':1},
+		effects:[
+			{type:'provide',what:{'burial spot':100}},
+			//{type:'waste',chance:1/100,desired:true},
+			{type:'function',func:function(me){
+				var buried=G.getRes('burial spot').used;
+				if (buried>0 && G.getRes('burial spot').amount>=buried)
+				{
+					var toDie=Math.min(me.amount,randomFloor(buried*0.001));
+					me.targetAmount-=toDie;
+					G.wasteUnit(me,toDie);
+					G.getRes('burial spot').amount-=toDie;
+					G.getRes('burial spot').used-=toDie;
+				}
+			}}
+		],
+		req:{'burial':true},
+		category:'civil',
+	});
 	
 }
 });
