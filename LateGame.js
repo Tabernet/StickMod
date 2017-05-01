d65b7bb9e06c89735473d7cf2a10fe7d2c208033
Tabernet <muppets@gmail.com>
2017-05-01 13:41:35 -0400

    Update LateGame.js

--- LateGame.js
+++ LateGame.js
@@ -5,7 +5,7 @@ desc:'An Econ mod that adds a later game uses to heavily stockpiled resources',
 engineVersion:1,
 manifest:'LateGame.js',
 requires:['Default dataset*'],
-sheets:{'LateGameSheet':'https://raw.githubusercontent.com/Tabernet/StickMod/master/Lategame.png'},
+sheets:{'LateGameSheet':'http://i.imgur.com/77Dpe5g.png'},
 func:function()
 {
 	//The idea in this mod is to add a means of adding use to items that otherwise just fill storage
